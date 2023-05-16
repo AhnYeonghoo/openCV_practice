903e05c12724298b7c3cd66fb806b790ca9bcf0b
AhnYeonghoo <dksdudgn00@naver.com>
2023-05-16 20:30:16 +0900

    Mat class Example

--- ch03/MapOp/main.cpp
+++ ch03/MapOp/main.cpp
@@ -5,12 +5,20 @@
 void MatOp1();
 void MatOp2();
 void MatOp3();
+void MatOp4();
+void MatOp5();
+void MatOp6();
+void MatOp7();
 
 int main()
 {
-	MatOp1();
+	//MatOp1();
 	//MatOp2();
-	MatOp3();
+	//MatOp3();
+	//MatOp4();
+	//MatOp5();
+	//MatOp6();
+	MatOp7();
 }
 
 void MatOp1()
@@ -88,4 +96,92 @@ void MatOp3()
 	cv::waitKey();
 	cv::destroyAllWindows();
 
+}
+
+void MatOp4()
+{
+	cv::Mat mat1 = cv::Mat::zeros(3, 4, CV_8UC1);
+	
+	for (int j = 0; j < mat1.rows; j++)
+	{
+		for (int i = 0; i < mat1.cols; i++)
+		{
+			mat1.at<uchar>(j, i)++;
+		}
+	}
+
+	for (int j = 0; j < mat1.rows; j++)
+	{
+		uchar* p = mat1.ptr<uchar>(j);
+		for (int i = 0; i < mat1.cols; i++)
+		{
+			p[i]++;
+		}
+	}
+	
+	for (cv::MatIterator_<uchar> it = mat1.begin<uchar>(); it != mat1.end<uchar>(); ++it)
+	{
+		(*it)++;
+	}
+
+	std::cout << "mat1:\n" << mat1 << std::endl;
+
+}
+
+void MatOp5()
+{
+	cv::Mat img1 = cv::imread("lenna.bmp");
+	
+	std::cout << "width: " << img1.cols << std::endl;
+	std::cout << "Height: " << img1.rows << std::endl;
+	std::cout << "Channels: " << img1.channels() << std::endl;
+	
+	if (img1.type() == CV_8UC1)
+	{
+		std::cout << "img5 is a grayscale image. " << std::endl;
+	} 
+	else if (img1.type() == CV_8UC3)
+	{
+		std::cout << "img5 is truecolor image." << std::endl;
+	}
+	
+	float data[] = { 2.f, 1.414f, 3.f, 1.732f };
+	cv::Mat mat1(2, 2, CV_32FC1, data);
+	std::cout << "mat1:\n" << mat1 << std::endl;
+}
+
+void MatOp6()
+{
+	float data[] = { 1, 1, 2, 3 };
+	cv::Mat mat1(2, 2, CV_32FC1, data);
+	std::cout << "mat1:\n" << mat1 << std::endl;
+	
+	cv::Mat mat2 = mat1.inv();
+	std::cout << "mat2:\n" << mat2 << std::endl;
+	
+	std::cout << "mat1.t(): \n" << mat1.t() << std::endl;
+	std::cout << "mat1 + mat2:\n" << mat1 + mat2 << std::endl;
+	std::cout << "mat1 * mat2:\n" << mat1 * mat2 << std::endl;
+}
+
+void MatOp7()
+{
+	cv::Mat img1 = cv::imread("lenna.bmp", cv::IMREAD_GRAYSCALE);
+	cv::Mat img1f;
+	
+	img1.convertTo(img1f, CV_32FC1);
+	
+	uchar data1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+	cv::Mat mat1(3, 4, CV_8UC1, data1);
+	cv::Mat mat2 = mat1.reshape(0, 1);
+	
+	std::cout << "Mat1:\n" << mat1 << std::endl;
+	std::cout << "Mat2:\n" << mat2 << std::endl;
+	
+	cv::Mat mat3 = cv::Mat::ones(1, 4, CV_8UC1) * 255;
+	mat1.push_back(mat3);
+	
+	std::cout << "mat1:\n" << mat1 << std::endl;
+	mat1.resize(6, 100);
+	std::cout << "mat1:\n" << mat1 << std::endl;
 }
