e917a8400a18fea2e487f749038ef7f8c0848a45
AhnYeonghoo <dksdudgn00@naver.com>
2023-05-23 08:49:59 +0900

    WebCam

---
+++ CV_ex_01/test.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+	VideoCapture cap(0);
+	if (!cap.isOpened())
+	{
+		cout << "Cant open the camera" << endl;
+		return -1;
+	}
+
+	Mat img;
+	
+	
+	while (1)
+	{
+		cap >> img;
+		imshow("camera img", img);
+		if (waitKey(1) == 27) break;
+	} 
+
+	return 0;
+}
