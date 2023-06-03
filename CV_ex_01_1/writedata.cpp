eba16e52fb12d6d8502ecae8c1101d8da06dfdae
AhnYeonghoo <dksdudgn00@naver.com>
2023-06-03 18:12:59 +0900

    Chapter7 Blurring

--- CV_ex_01_1/writedata.cpp
+++ CV_ex_01_1/writedata.cpp
@@ -444,6 +444,135 @@ void blurringMean()
 	}
 	destroyAllWindows();
 }
+
+void blurringGaussian()
+{
+	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+	if (src.empty())
+	{
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+	imshow("src", src);
+	Mat dst;
+	for (int sigma = 1; sigma <= 5; sigma++)
+	{
+		GaussianBlur(src, dst, Size(), (double)sigma);
+
+		String text = format("sigma = %d", sigma);
+		putText(dst, text, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0,
+			Scalar(255), 1, LINE_AA);
+		imshow("dst", dst);
+		waitKey();
+	}
+
+	destroyAllWindows();
+	
+}
+
+void unsharpMask()
+{
+	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+	if (src.empty())
+	{
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+	imshow("src", src);
+	for (int i = 1; i <= 5; i++)
+	{
+		Mat blurred;
+		GaussianBlur(src, blurred, Size(), i);
+		float alpha = 1.f;
+		Mat dst = (1 + alpha) * src - alpha * blurred;
+		String desc = format("sigma: %d", i);
+		putText(dst, desc, Point(10, 30), FONT_HERSHEY_COMPLEX,
+			1.0, Scalar(255), 1, LINE_AA);
+		imshow("dst", dst);
+		waitKey();
+	}
+	destroyAllWindows();
+}
+
+void noise()
+{
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+	if (src.empty())
+	{
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+	imshow("src", src);
+	for (int i = 10; i <= 30; i += 10)
+	{
+		Mat noise(src.size(), CV_32SC1);
+		randn(noise, 0, i);
+		Mat dst;
+		add(src, noise, dst, Mat(), CV_8U);
+		String desc = format("i=%d", i);
+		putText(dst, desc, Point(10, 30), FONT_HERSHEY_COMPLEX, 1.0,
+			Scalar(255), 1, LINE_AA);
+		imshow("dst", dst);
+		waitKey();
+	}
+	destroyAllWindows();
+}
+
+void filterBilareral()
+{
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+	if (src.empty())
+	{
+		cerr << "image load failed!" << endl;
+		return;
+	}
+	
+	Mat noise(src.size(), CV_32SC1);
+	randn(noise, 0, 5);
+	add(src, noise, src, Mat(), CV_8U);
+
+	Mat dst1;
+	GaussianBlur(src, dst1, Size(), 5);
+
+	Mat dst2;
+	bilateralFilter(src, dst2, -1, 10, 5);
+	
+	imshow("src", src);
+	imshow("dst1", dst1);
+	imshow("dst2", dst2);
+	waitKey();
+	destroyAllWindows();
+
+}
+
+void filterMedian()
+{
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+	if (src.empty())
+	{
+		cerr << "image load failed!" << endl;
+		return;
+	}
+	int num = (int)(src.total() * 0.1);
+	for (int i = 0; i < num; i++)
+	{
+		int x = rand() % src.cols;
+		int y = rand() % src.rows;
+		src.at<uchar>(y, x) = (i % 2) * 255;
+	}
+
+	Mat dst1;
+	GaussianBlur(src, dst1, Size(), 1);
+	Mat dst2;
+	medianBlur(src, dst2, 3);
+	imshow("src", src);
+	imshow("dst1", dst1);
+	imshow("dst2", dst2);
+	waitKey();
+	destroyAllWindows();
+
+
+}
 int main()
 {
 
@@ -463,6 +592,11 @@ int main()
 	//logical();
 	//filterEmbossing();
 	//blurringMean();
+	//blurringGaussian();
+	//unsharpMask();
+	//noise();
+	//filterBilareral();
+	filterMedian();
 
 	return 0;
 }
