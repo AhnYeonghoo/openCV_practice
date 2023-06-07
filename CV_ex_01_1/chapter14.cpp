1f828a0c63a6d5ed794637405429cbc62865e500
AhnYeonghoo <dksdudgn00@naver.com>
2023-06-07 14:02:39 +0900

    good mathing keypoint

--- CV_ex_01_1/chapter14.cpp
+++ CV_ex_01_1/chapter14.cpp
@@ -139,6 +139,42 @@ void keypointMatching()
 	destroyAllWindows();
 }
 
+void goodMatching()
+{
+	Mat src1 = imread("box.png", IMREAD_GRAYSCALE);
+	Mat src2 = imread("box_in_scene.png", IMREAD_GRAYSCALE);
+	
+	if (src1.empty() || src2.empty())
+	{
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+
+	Ptr<Feature2D> feature = ORB::create();
+	vector<KeyPoint> keypoints1, keypoints2;
+	
+	Mat desc1, desc2;
+	feature->detectAndCompute(src1, Mat(), keypoints1, desc1);
+	feature->detectAndCompute(src2, Mat(), keypoints2, desc2);
+	
+	Ptr<DescriptorMatcher> matcher = BFMatcher::create(NORM_HAMMING);
+	
+	vector<DMatch> matches;
+	matcher->match(desc1, desc2, matches);
+	
+	std::sort(matches.begin(), matches.end());
+	vector<DMatch> good_matches(matches.begin(), matches.begin() + 50);
+
+	Mat dst;
+	drawMatches(src1, keypoints1, src2, keypoints2, good_matches, dst,
+		Scalar::all(-1), Scalar::all(-1), vector<char>(),
+		DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+	imshow("dst", dst);
+	waitKey();
+	destroyAllWindows();
+}
+
 int main()
 {
 	//corner_harris();
